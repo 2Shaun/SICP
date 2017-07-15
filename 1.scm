a35c9405619866288f43ca54e36853b1c93718dc
tomoshaughnessy <tomoshaughnessy3492@gmail.com>
2017-07-14 21:41:47 -0400

    Exercise 1.19

--- 1.scm
+++ 1.scm
@@ -184,3 +184,20 @@
 	  ((even? counter) (iter (+ product (double b)) (- counter 2)))
 	  (else (iter (+ product b) (- counter 1)))))
   (iter 0 n))
+
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+	((even? count)
+	 (fib-iter a
+		   b
+		   (+ (square p) (square q))
+		   (+ (square q) (* 2 p q))
+		   (/ count 2)))
+	(else (fib-iter (+ (* b q) (* a q) (* a p))
+			(+ (* b p) (* a q))
+			p
+			q
+			(- count 1)))))
