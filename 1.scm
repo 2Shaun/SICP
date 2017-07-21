601a0c6ce15343ee2a2362319d9e5e6e839b2b44
tomoshaughnessy <tomoshaughnessy3492@gmail.com>
2017-07-20 20:34:05 -0400

    Exercise 1.22

--- 1.scm
+++ 1.scm
@@ -201,3 +201,39 @@
 			p
 			q
 			(- count 1)))))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+	((divides? test-divisor n) test-divisor)
+	(else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (search-for-primes lb ub)
+  (define (iter odd-lb-iter)
+    (cond ((<= odd-lb-iter ub)
+	   (timed-prime-test odd-lb-iter)
+	   (iter (+ odd-lb-iter 2)))))
+    (if (even? lb)
+	(iter (+ lb 1))
+      (iter lb)))
